1e1681a065234bfa374ef4c3c3521c4fae16cdf4
Felipe M3nin0 <felipe.carlos@fatec.sp.gov.br>
2020-02-21 17:44:47 -0300

    Update env variables in Dockerfile and plugin install script

--- ckan/install_plugins.sh
+++ ckan/install_plugins.sh
@@ -4,20 +4,24 @@
 # https://github.com/NaturalHistoryMuseum/ckanext-doi
 #####
 
-## Download and install ckanext-doi
-cd $INSTALL_FOLDER/src && \
-    git clone https://github.com/NaturalHistoryMuseum/ckanext-doi.git && \
-    . $INSTALL_FOLDER/bin/activate && \
-    cd $INSTALL_FOLDER/src/ckanext-doi && \
-    pip install -r requirements.txt && \
-    cd $INSTALL_FOLDER/src/ckanext-doi && \
-    python setup.py develop
+if [ $DOI_EXT_PLUGIN -eq 1 ]
+then
+    ## Download and install ckanext-doi
+    cd $CKAN_VENV/src && \
+        git clone https://github.com/NaturalHistoryMuseum/ckanext-doi.git && \
+        . $CKAN_VENV/bin/activate && \
+        cd $CKAN_VENV/src/ckanext-doi && \
+        pip install -r requirements.txt && \
+        cd $CKAN_VENV/src/ckanext-doi && \
+        python setup.py develop
 
-# Configuring production
-sed -i '/ckan.plugins/s/$/ doi/' production.ini
+    # Configuring production
+    sed -i '/ckan.plugins/s/$/ doi/' $CKAN_CONFIG/production.ini
 
-sed -i '/Front-End Settings/ i\ckanext.doi.account_name = '$DOI_EXT_ACCOUNT_NAME'' production.ini
-sed -i '/Front-End Settings/ i\ckanext.doi.account_password = '$DOI_EXT_ACCOUNT_PASSWORD'' production.ini
-sed -i '/Front-End Settings/ i\ckanext.doi.prefix = '$DOI_EXT_PREFIX'' production.ini
-sed -i '/Front-End Settings/ i\ckanext.doi.publisher = '$DOI_EXT_PUBLISHER'' production.ini
-sed -i '/Front-End Settings/ i\ckanext.doi.test_mode = '$DOI_EXT_TEST_MODE'' production.ini
+    sed -i '/Front-End Settings/ i\## ckanext-doi Settings' $CKAN_CONFIG/production.ini
+    sed -i '/Front-End Settings/ i\ckanext.doi.account_name = '$DOI_EXT_ACCOUNT_NAME'' $CKAN_CONFIG/production.ini
+    sed -i '/Front-End Settings/ i\ckanext.doi.account_password = '$DOI_EXT_ACCOUNT_PASSWORD'' $CKAN_CONFIG/production.ini
+    sed -i '/Front-End Settings/ i\ckanext.doi.prefix = '$DOI_EXT_PREFIX'' $CKAN_CONFIG/production.ini
+    sed -i '/Front-End Settings/ i\ckanext.doi.publisher = '$DOI_EXT_PUBLISHER'' $CKAN_CONFIG/production.ini
+    sed -i '/Front-End Settings/ i\ckanext.doi.test_mode = '$DOI_EXT_TEST_MODE'' $CKAN_CONFIG/production.ini
+fi

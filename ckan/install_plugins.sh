9d3328556a015a65e3eacbfa3568295d51bf7b24
GSansigolo <gabrielsansigolo@gmail.com>
2020-05-19 17:57:29 -0300

    add userdataset plugin

--- ckan/install_plugins.sh
+++ ckan/install_plugins.sh
@@ -27,3 +27,27 @@ then
     sed -i '/Front-End Settings/ i\ckanext.doi.publisher = '$DOI_EXT_PUBLISHER'' $CKAN_CONFIG/production.ini
     sed -i '/Front-End Settings/ i\ckanext.doi.test_mode = '$DOI_EXT_TEST_MODE'' $CKAN_CONFIG/production.ini
 fi
+
+####
+# Script to install ckanext-doi
+# https://github.com/NaturalHistoryMuseum/ckanext-userdatasets
+#
+#####
+if [ $USERDATA_EXT_PLUGIN -eq 1 ] && [ ! -d "$CKAN_VENV/src/ckanext-userdatasets" ]
+then
+
+    ## Download and install ckanext-userdatasets
+    cd $CKAN_VENV/src && \
+        git clone https://github.com/NaturalHistoryMuseum/ckanext-userdatasets.git && \
+        . $CKAN_VENV/bin/activate && \
+        cd $CKAN_VENV/src/ckanext-userdatasets && \
+        pip install -r requirements.txt && \
+        cd $CKAN_VENV/src/ckanext-userdatasets && \
+        python setup.py develop
+
+    # Configuring production.ini
+    sed -i '/ckan.plugins/s/$/ userdatasets/' $CKAN_CONFIG/production.ini
+    sed -i '/Front-End Settings/ i\ckanext.userdatasets.default_auth_module = '$USERDATA_AUTH_MODULE'' $CKAN_CONFIG/production.ini
+    sed -i '/Front-End Settings/ i\ckanext.userdatasets.default_action_module = '$USERDATA_ACTION_MODULE'' $CKAN_CONFIG/production.ini
+ 
+fi
